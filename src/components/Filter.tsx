be1152d6d5c0d124960e95b02b033a5b67202893
Sameer Amesara <62166762+SameerAmesara@users.noreply.github.com>
2024-02-08 00:12:57 -0400

    Update Filter.tsx

--- src/components/Filter.tsx
+++ src/components/Filter.tsx
@@ -1,4 +1,3 @@
-import { useState } from "react";
 import Box from "@mui/material/Box";
 import Typography from "@mui/material/Typography";
 import Slider from "@mui/material/Slider";
@@ -11,51 +10,17 @@ import {
   FormGroup,
 } from "@mui/material";
 
-interface FilterValues {
-  areaOfExpertise: string;
-  experience: number;
-  rating: number;
-}
-
 function Filter() {
-  const [filterValues, setFilterValues] = useState<FilterValues>({
-    areaOfExpertise: "",
-    experience: 3,
-    rating: 0,
-  });
-
-  const handleExperienceChange = (
-    event: Event,
-    newValue: number | number[]
-  ) => {
-    setFilterValues({
-      ...filterValues,
-      experience: newValue as number,
-    });
-  };
-
-  const handleRatingChange = (event: Event, newValue: number | number[]) => {
-    setFilterValues({
-      ...filterValues,
-      rating: newValue as number,
-    });
-  };
-
   return (
     <Box sx={{ p: 2, width: "100%", minWidth: 200 }}>
       <Typography variant="h6">Filters</Typography>
       <FormGroup>
         <FormControl variant="outlined" sx={{ mt: 2 }}>
           <InputLabel>Area of Expertise</InputLabel>
-          <Select
-            value={filterValues.areaOfExpertise}
-            label="Area of Expertise"
-          >
-            {/* Populate this list with actual areas of expertise */}
+          <Select label="Area of Expertise">
             <MenuItem value="Java">Java</MenuItem>
             <MenuItem value="Python">Python</MenuItem>
             <MenuItem value="JavaScript">JavaScript</MenuItem>
-            {/* ... other options */}
           </Select>
         </FormControl>
 
@@ -63,8 +28,7 @@ function Filter() {
           Professional Experience
         </Typography>
         <Slider
-          value={filterValues.experience}
-          onChange={handleExperienceChange}
+          defaultValue={2}
           aria-labelledby="experience-slider"
           valueLabelDisplay="auto"
           step={1}
@@ -77,8 +41,7 @@ function Filter() {
           Ratings
         </Typography>
         <Slider
-          value={filterValues.rating}
-          onChange={handleRatingChange}
+          defaultValue={1}
           aria-labelledby="rating-slider"
           valueLabelDisplay="auto"
           step={1}
